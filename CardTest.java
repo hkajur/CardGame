6154ca573824fbcde1733671e714da84aca691c0
Harish Kajur <hkajur93@gmail.com>
2014-11-11 23:16:29 -0500

    Update the Card test java file

--- CardTest.java
+++ CardTest.java
@@ -1,241 +1,34 @@
 import java.io.*;
 import java.util.*;
 
-enum CardValue {
-        ACE, 
-        TWO, 
-        THREE, 
-        FOUR, 
-        FIVE, 
-        SIX, 
-        SEVEN, 
-        EIGHT, 
-        NINE, 
-        TEN, 
-        JOKER, 
-        QUEEN, 
-        KING
-}
-
-enum CardType {
-        HEARTS, 
-        DIAMONDS, 
-        SPADES, 
-        CLUBS
-}
-
-class CardTypeException extends Exception {
-   
-    public CardTypeException(){
-        System.err.println("Exception: Invalid Card type");
-    }
-}
-
-class CardValueException extends Exception {
-   
-    public CardValueException(){
-        System.err.println("Exception: Invalid Card type");
-    }
-}
-
-class Card implements Comparable<Card>{
-    
-    private String color;
-    private CardType cardType;   
-    private CardValue cardValue;
-
-    public Card(){}
-
-    public Card(String color, CardType cardType, CardValue cardValue){
-        setColor(color);
-        setCardType(cardType);
-        setCardValue(cardValue);
-    }
-
-    private void setColor(String color){
-        this.color = color;
-    }
-    
-    private void setCardType(CardType cardType){
-        this.cardType = cardType;
-    }
-    
-    private void setCardValue(CardValue cardValue){
-        this.cardValue = cardValue;
-    }
-
-    public String getColor(){
-        return color;
-    }
-
-    public String getCardType(){
-        
-        String temp = "";
-
-        switch(cardType){
-            case HEARTS:
-                temp = "Hearts";
-                break;
-            case DIAMONDS:
-                temp = "Diamonds";
-                break;
-            case SPADES:
-                temp = "Spades";
-                break;
-            case CLUBS:
-                temp = "Clubs";
-                break;
-            default:
-                temp = "";
-                break;
-        }
-
-        return temp;
-    }
-    
-    public int getCardValueNum(){
-        
-        int temp = 0;
-
-        switch(cardValue){
-            case ACE:
-                temp = 1;
-                break;
-            case TWO:
-                temp = 2;
-                break;
-            case THREE:
-                temp = 3;
-                break;
-            case FOUR:
-                temp = 4;
-                break;
-            case FIVE:
-                temp = 5;
-                break;
-            case SIX:
-                temp = 6;
-                break;
-            case SEVEN:
-                temp = 7;
-                break;
-            case EIGHT:
-                temp = 8;
-                break;
-            case NINE:
-                temp = 9;
-                break;
-            case TEN:
-                temp = 10;
-                break;
-            case JOKER:
-                temp = 11;
-                break;
-            case QUEEN:
-                temp = 12;
-                break;
-            case KING:
-                temp = 13;
-                break;
-            default:
-                temp = 0;
-                break;
-       }
-
-       return temp;
-    }
-
-    public String getCardValue(){
-        
-        String temp = "";
-
-        switch(cardValue){
-            case ACE:
-                temp = "1";
-                break;
-            case TWO:
-                temp = "2";
-                break;
-            case THREE:
-                temp = "3";
-                break;
-            case FOUR:
-                temp = "4";
-                break;
-            case FIVE:
-                temp = "5";
-                break;
-            case SIX:
-                temp = "6";
-                break;
-            case SEVEN:
-                temp = "7";
-                break;
-            case EIGHT:
-                temp = "8";
-                break;
-            case NINE:
-                temp = "9";
-                break;
-            case TEN:
-                temp = "10";
-                break;
-            case JOKER:
-                temp = "Joker";
-                break;
-            case QUEEN:
-                temp = "Queen";
-                break;
-            case KING:
-                temp = "King";
-                break;
-            default:
-                temp = "";
-                break;
-       }
-
-       return temp;
-    }
-
-    public int compareTo(Card card){
-        
-        int cardValue = this.getCardValueNum();
-        int otherCardValue = card.getCardValueNum();
-
-        if(cardValue > otherCardValue)
-            return 1;
-        else if(cardValue < otherCardValue)
-            return -1;
-        else
-            return 0;
-    }
-    
-    public String toString(){
-        return getCardValue() + " of " + getColor() + " " + getCardType();
-    }
-}
-
 public class CardTest {
 
     public static List<Card> cardDeck;
 
+    // Generate a random number specified from the given min, max
+
     public static int generateRandomNum(int min, int max){
+    
         int range = (max - min) + 1;
         int temp = (int)(Math.random() * range) + min;
         
         return temp;
     }
 
+    // Get a random card from the deck as well as remove 
+    // card because that card is not in the deck anymore
+
     public static Card getRandomCard(){
-        
-        int num = generateRandomNum(0, cardDeck.size()-1);
+       
+        int max = cardDeck.size() - 1;
+        int num = generateRandomNum(0, max);
     
         Card card = (Card) cardDeck.remove(num);
         
         return card;
     }
 
-    public static void main(String[] args){
+    public static void createDeck(){
 
         cardDeck = new ArrayList<Card>();
 
@@ -250,12 +43,13 @@ public class CardTest {
         
         for(CardValue cardValue: CardValue.values())
             cardDeck.add(new Card("Black", CardType.CLUBS, cardValue));
-   
 
-        int i = 0;
-        
-        do {
+    }
+
+    public static void drawCards(int num){
             
+        for(int i = 0; i < num; i++){ 
+        
             Card userCard = getRandomCard();
             Card computerCard = getRandomCard();
 
@@ -271,11 +65,17 @@ public class CardTest {
             else 
                 System.out.println("No one won");
 
-            i++;
+        }
+    }
 
-        } while(i < 6);
+    public static void printDeck(){
+        for(Card card: cardDeck)
+            System.out.println(card);
+    }
 
-        //for(Card card : cardDeck)
-       //     System.out.println(card);
+    public static void main(String[] args){
+        createDeck(); 
+        drawCards(2);
+        //printDeck();
     }
 }
